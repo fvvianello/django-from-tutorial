1791918504dc6e82ee8e95e11615ff79010e6efc
Francesca Vianello <francesca.vianello11@imperial.ac.uk>
2019-03-11 17:11:43 +0000

    Started poll server and added question choices

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Choice,Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None,                  {'fields': ['question_text']}),
+    ('Date information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
